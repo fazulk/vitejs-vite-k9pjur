25b45d265b1ae63a3afc022ba38915cd8fa7bbad
fazulk <fazulk@gmail.com>
2024-11-13 14:31:07 -0800

    stop

--- release.sh
+++ release.sh
@@ -2,10 +2,10 @@
 
 git checkout main
 
-if [ $? -ne 0 ]; then
-    echo "Failed to checkout main branch. Aborting."
-    exit 1
-fi
+# if [ $? -ne 0 ]; then
+#     echo "Failed to checkout main branch. Aborting."
+#     exit 1
+# fi
 
 git pull origin main
 
